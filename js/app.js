695baff07ca0c0ee8468b0572f6e9c5c32935afe
Muhammed Aydın <muhammedaydiiinnn@gmail.com>
2026-04-07 21:13:36 +0300

    connect state with physics calculations

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { addObject, state } from "./state.js";
+
 const plankElement = document.getElementById("plank");
 const leftWeightElement = document.getElementById("leftWeight");
 const rightWeightElement = document.getElementById("rightWeight");
@@ -14,6 +16,20 @@ function initApp() {
     angleValueElement,
     resetButtonElement,
   });
+
+  addObject({
+    id: "test-1",
+    weight: 5,
+    position: 100,
+  });
+
+  addObject({
+    id: "test-2",
+    weight: 8,
+    position: 300,
+  });
+
+  console.log("Current state:", state);
 }
 
 initApp();
